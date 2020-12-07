07e2ea42a37103227333833be9a4eb1eb2e101ef
Jimmy McAvoy <jimmymcavoy@localhost.localdomain>
2020-12-06 22:46:45 -0500

    Working command line arguments

--- characterCount.py
+++ characterCount.py
@@ -1,1 +1,20 @@
 #! /usr/bin/python
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='File and search input')
+
+parser.add_argument("--f")
+
+parser.add_argument("--s")
+
+args = parser.parse_args()
+
+f = args.f
+
+s = args.s
+
+print(f)
+
+print(s)
